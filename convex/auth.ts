cb2308827d2108b86135cabfee6140ab3491ae41
whiiteechocolatee <creatuseandr@icloud.com>
2024-09-09 21:32:27 +0300

    create custom password flow with ability to add name to signup

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,7 +1,17 @@
-import { convexAuth } from '@convex-dev/auth/server';
 import Google from '@auth/core/providers/google';
 import { Password } from '@convex-dev/auth/providers/Password';
+import { convexAuth } from '@convex-dev/auth/server';
+import { DataModel } from './_generated/dataModel.d';
+
+const CustomPassword = Password<DataModel>({
+  profile(params) {
+    return {
+      email: params.email as string,
+      name: params.name as string,
+    };
+  },
+});
 
 export const { auth, signIn, signOut, store } = convexAuth({
-  providers: [Google, Password],
+  providers: [Google, CustomPassword],
 });
