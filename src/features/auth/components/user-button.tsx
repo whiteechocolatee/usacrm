8f22107b7a0c2eb7086deaea3a3c22ef4ffc1635
whiiteechocolatee <creatuseandr@icloud.com>
2024-09-09 21:33:20 +0300

    create user button component for logged in users

---
+++ src/features/auth/components/user-button.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { useAuthActions } from '@convex-dev/auth/react';
+import { Loader, LogOut } from 'lucide-react';
+import { useCurrentUser } from '../hooks/use-current-user';
+
+function UserButton() {
+  const { signOut } = useAuthActions();
+  const { data, isLoading } = useCurrentUser();
+
+  if (isLoading) {
+    return <Loader className="size-4 animate-spin text-muted-foreground" />;
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  const { image, name } = data;
+
+  const avatarFallback = name!.charAt(0).toUpperCase();
+
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger className="outine-none relative">
+        <Avatar className="size-10 hover:opacity-75 transition">
+          <AvatarImage src={image} alt={name} />
+          <AvatarFallback className="bg-sky-500 text-white">
+            {avatarFallback}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="center" side="right" className="w-60">
+        <DropdownMenuItem onClick={signOut}>
+          <LogOut className="size-4 mr-2" />
+          Log Out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default UserButton;
