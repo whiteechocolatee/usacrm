0a0e394bfbc3dec150ea99040965838d6c8b7bab
whiiteechocolatee <creatuseandr@icloud.com>
2024-09-14 12:53:17 +0300

    check if user already have workspace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
 'use client';
 
 import UserButton from '@/features/auth/components/user-button';
+import { useGetWorkspaces } from '@/features/workspaces/api/use-get-workspaces';
+import { useCreateWorkspaceModal } from '@/features/workspaces/store/use-create-workspace-modal';
+import { useRouter } from 'next/navigation';
+import { useEffect, useMemo } from 'react';
 
 export default function Home() {
+  const [open, setOpen] = useCreateWorkspaceModal();
+  const router = useRouter();
+
+  const { data, isLoading } = useGetWorkspaces();
+
+  // eslint-disable-next-line
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    if (workspaceId) {
+      router.replace(`/workspace/${workspaceId}`);
+    } else if (!open) {
+      setOpen(true);
+    }
+  }, [workspaceId, isLoading, setOpen, open, router]);
+
   return (
     <div>
       <UserButton />
