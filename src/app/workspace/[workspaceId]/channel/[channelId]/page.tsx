6167cae998ba1f16743e4bd3daaf9d9ec040a5c4
whiiteechocolatee <creatuseandr@icloud.com>
2024-09-23 13:52:58 +0300

    get all messages and pass to messages list

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import MessageList from '@/components/message-list';
 import { useGetChannel } from '@/features/channels/api/use-get-channel';
 import { useGetMessages } from '@/features/messages/api/use-get-message';
 import { useChannelId } from '@/hooks/use-channel-id';
@@ -10,14 +11,14 @@ import ChatInput from './chat-input';
 function ChannelIdPage() {
   const channelId = useChannelId();
 
-  const { results } = useGetMessages({
+  const { results, status, loadMore } = useGetMessages({
     channelId,
   });
   const { data: channel, isLoading: channelLoading } = useGetChannel({
     id: channelId,
   });
 
-  if (channelLoading) {
+  if (channelLoading || status === 'LoadingFirstPage') {
     return (
       <div className="h-full flex-1 flex items-center justify-center">
         <Loader className="size-5 animate-spin shrink-0 text-muted-foreground" />
@@ -37,7 +38,15 @@ function ChannelIdPage() {
   return (
     <div className="flex flex-col h-full">
       <Header title={channel.name} />
-      <div className="flex-1">{JSON.stringify(results)}</div>
+      <MessageList
+        channelName={channel.name}
+        // eslint-disable-next-line
+        channelCreationTime={channel._creationTime}
+        data={results}
+        loadMore={loadMore}
+        isLoadingMore={status === 'LoadingMore'}
+        canLoadMore={status === 'CanLoadMore'}
+      />
 
       <ChatInput
         placeholder={`Type a message to "${channel.name.split('-').join(' ')}"...`}
