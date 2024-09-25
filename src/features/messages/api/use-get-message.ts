0fc8604c9aede63139483f377a9bdb990ce506cc
whiiteechocolatee <creatuseandr@icloud.com>
2024-09-25 13:47:19 +0300

    hooks : get single message, get messages , use parent message id

--- src/features/messages/api/use-get-message.ts
+++ src/features/messages/api/use-get-message.ts
@@ -1,38 +1,15 @@
-import { usePaginatedQuery } from 'convex/react';
+import { useQuery } from 'convex/react';
 import { Id } from '../../../../convex/_generated/dataModel';
 import { api } from '../../../../convex/_generated/api';
 
-const BATCH_SIZE = 20;
-
-interface UseGetMessagesProps {
-  channelId?: Id<'channels'>;
-  conversationId?: Id<'conversations'>;
-  parentMessageId?: Id<'messages'>;
+interface UseGetMessageProps {
+  id: Id<'messages'>;
 }
 
-export type GetMessagesReturnType =
-  (typeof api.messages.get._returnType)['page'];
+export const useGetMessage = ({ id }: UseGetMessageProps) => {
+  const data = useQuery(api.messages.getById, { id });
 
-export const useGetMessages = ({
-  channelId,
-  conversationId,
-  parentMessageId,
-}: UseGetMessagesProps) => {
-  const { results, status, loadMore } = usePaginatedQuery(
-    api.messages.get,
-    {
-      channelId,
-      conversationId,
-      parentMessageId,
-    },
-    {
-      initialNumItems: BATCH_SIZE,
-    },
-  );
+  const isLoading = data === undefined;
 
-  return {
-    results: results as GetMessagesReturnType,
-    status,
-    loadMore: () => loadMore(BATCH_SIZE),
-  };
+  return { data, isLoading };
 };
