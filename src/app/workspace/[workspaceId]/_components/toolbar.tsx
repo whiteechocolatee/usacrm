c1976f4bd4ae122210c415d5da1e1c1183f54fe2
whiiteechocolatee <creatuseandr@icloud.com>
2024-09-27 13:02:37 +0300

    update search toolbar with command search

--- src/app/workspace/[workspaceId]/_components/toolbar.tsx
+++ src/app/workspace/[workspaceId]/_components/toolbar.tsx
@@ -1,24 +1,91 @@
 import { Button } from '@/components/ui/button';
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from '@/components/ui/command';
+import { useGetChannels } from '@/features/channels/api/use-get-channels';
+import { useGetMembers } from '@/features/channels/api/use-get-members';
 import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspaces-by-id';
 import { useWorkspaceId } from '@/hooks/use-workspace-id';
 import { Info, Search } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 function Toolbar() {
+  const router = useRouter();
   const workspaceId = useWorkspaceId();
+  const [open, setOpen] = useState(false);
 
   const { data } = useGetWorkspace({ id: workspaceId });
+  const { data: channels } = useGetChannels({ workspaceId });
+  const { data: members } = useGetMembers({ workspaceId });
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === 'j' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen(prevOpen => !prevOpen);
+      }
+    };
+
+    document.addEventListener('keydown', down);
+    return () => document.removeEventListener('keydown', down);
+  }, []);
+
+  const onChannelClick = (channelId: string) => {
+    setOpen(false);
+    router.push(`/workspace/${workspaceId}/channel/${channelId}`);
+  };
+
+  const onMemberClick = (memberId: string) => {
+    setOpen(false);
+    router.push(`/workspace/${workspaceId}/member/${memberId}`);
+  };
 
   return (
     <nav className="bg-[#481349] flex w-full items-center justify-between h-10 p-1.5">
       <div className="flex-1" />
       <div className="min-w-[280px] max-w-[642px] grow-[2] shrink">
         <Button
+          onClick={() => setOpen(true)}
           size="sm"
           className="bg-accent/25 hover:bg-accent/25 w-full justify-start h-7 px-2"
         >
           <Search className="size-4 text-white mr-2" />
-          <span className="text-white text-xs">Search {data?.name}</span>
+          <span className="text-white text-xs">Search in #{data?.name}</span>
         </Button>
+        <CommandDialog open={open} onOpenChange={setOpen}>
+          <CommandInput placeholder="Type a command or search..." />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup heading="Channels">
+              {channels?.map(channel => (
+                <CommandItem
+                  key={channel._id}
+                  onSelect={() => onChannelClick(channel._id)}
+                >
+                  {channel.name}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup heading="Members">
+              {members?.map(member => (
+                <CommandItem
+                  key={member._id}
+                  onSelect={() => onMemberClick(member._id)}
+                >
+                  {member.user.name}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </CommandDialog>
       </div>
       <div className="ml-auto flex-1 flex items-center justify-end">
         <Button variant="transparent" size="iconSm">
